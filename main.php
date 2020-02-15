5353fd1150119a446669a76aca0f8c68ffa133a2
colibri-artjom <59999093+colibri-artjom@users.noreply.github.com>
2020-02-15 12:29:30 +0200

    new update

--- main.php
+++ main.php
@@ -11,14 +11,19 @@ $m = 1;
  $col_2 = $columns->addColumn(10);
  $col_3 = $columns->addColumn(3);
 
- $clicker = $col_2->add(["Button",$_SESSION["i"],"green fluid big"]);
- $clicker->on('click', function ($m) {
-   $_SESSION["i"] = $_SESSION["i"] + 1;
-   return $m;
- });
+
+$val = $col_2->add(['FormField/Line', '1']);
+ $clicker = $col_2->add(["Button","Click!!!","green fluid big"]);
+$clicker->js('click', new \atk4\ui\jsReload($val, ['val' => $val->jsInput()->val(new \atk4\ui\jsExpression('parseInt([])+1', [$val->jsInput()->val()]), $val->jsInput()->focus())]));
  $label = $col_2->add(["Label",$_SESSION['user_id']]);
 
 $save = $col_2->add(["Button","Save","blue big"]);
 
 $exit = $app->add(['Button',"Ēxit","red"]);
-$exit->link(["exit"]);
+$exit->link(['exit']);
+
+$x2 = $col_3->add(["Button","click x2","medium white"]);
+
+$pus = $col_3->add(["Button","+0.5 click/sec","medium blue"]);
+
+$click1 = $col_3->add(["Button","+1 click/sec","medium red"]);
